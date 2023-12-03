f8dab62ce931520414fa78eac3c106b30efb8b2e
IrisWangAU <wxniris@hotmail.com>
2023-12-03 18:34:51 +1100

    amend entries in the database

---
+++ Hospital/dm.sql
@@ -0,0 +1,44 @@
+/* ITO4132 MO 2022 TP3 Assignment 2 Task 1 Part B ANSWERS
+   Student Name:
+   Student ID:
+
+   Comments to your marker:
+   
+*/
+
+/* (i)*/
+
+
+
+
+
+
+/* (ii)*/
+
+
+
+
+
+
+
+
+/* (iii)*/
+
+
+
+
+
+
+
+      
+/* (iv)*/
+
+
+
+
+
+
+
+
+
+
