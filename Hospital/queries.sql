8e7a3fd48340b346588218969c6ebcce8cac06fb
IrisWangAU <wxniris@hotmail.com>
2023-12-03 18:37:38 +1100

    sample SQL queries

---
+++ Hospital/queries.sql
@@ -0,0 +1,81 @@
+/* ITO4132 MO 2022 TP3 Assignment 2 Task 2 ANSWERS
+   Student Name:
+   Student ID:
+
+   Comments to your marker:
+   
+*/
+
+/* (i)*/
+
+
+
+
+
+
+
+/* (ii)*/
+
+
+
+
+
+
+
+    
+/* (iii)*/
+
+
+
+
+
+
+
+
+/* (iv)*/
+
+
+
+
+
+
+
+ 
+/* (v)*/
+
+
+
+
+
+
+
+    
+/* (vi)*/
+
+
+
+
+
+
+
+
+    
+/* (vii)*/
+
+
+
+
+
+
+
+
+    
+/* (viii)*/
+
+
+
+
+
+
+
+
